535bd63af0590c7a91695a04d194c569939e5f29
pedrolmr <pedroluismontesinosrivera@gmail.com>
2020-04-17 22:12:57 -0400

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <p>Hello</p>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Navbar />
+      </div>
+    );
+  }
 }
 
 export default App;
