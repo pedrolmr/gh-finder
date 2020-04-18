05b0b0686c06d3b68585d2ebd91baaacee746bf0
pedrolmr <pedroluismontesinosrivera@gmail.com>
2020-04-18 01:08:31 -0400

    fetching data from gh api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
+
+import axios from 'axios';
 
 import './App.css';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get('https://api.github.com/users');
+    this.setState({ users: res.data, loading: false });
+  }
   render() {
     return (
       <div className='App'>
         <Navbar />
+        <div className='container'>
+          <Users users={this.state.users} loading={this.state.loading} />
+        </div>
       </div>
     );
   }
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,22 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class Navbar extends Component {
-  static defaultProps = {
-    title: 'GH Finder',
-  };
+const Navbar = (props) => {
+  return (
+    <nav className='navbar bg-primary'>
+      <h1>{props.title}</h1>
+    </nav>
+  );
+};
 
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-  };
+Navbar.defaultProps = {
+  title: 'GH Finder',
+};
 
-  render() {
-    return (
-      <nav className='navbar bg-primary'>
-        <h1>{this.props.title}</h1>
-      </nav>
-    );
-  }
-}
-
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+};
 export default Navbar;
